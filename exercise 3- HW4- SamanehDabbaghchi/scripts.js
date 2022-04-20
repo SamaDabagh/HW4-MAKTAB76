0b682286c4a819b0b3139e2cd112aad32f8b1948
SAMDAB <s.dabbaghchi@gmail.com>
2022-04-20 23:28:19 +0430

    Solved Exercise 2

---
+++ exercise 3- HW4- SamanehDabbaghchi/scripts.js
@@ -0,0 +1,17 @@
+/* Ex.3
+Write a JavaScript function that accepts a string as a
+parameter and converts the first letter of each word of the
+string in upper case.
+Example string: 'the quick brown fox'
+Expected Output: 'The Quick Brown Fox '
+*/
+const upercaseOfFirstChar = function (str) {
+  return str
+    .split(" ")
+    .map((element, index) => {
+      return element.split("")[0].toUpperCase() + element.slice(1).toString();
+    })
+    .join(" ");
+};
+let newString = "the quick brown fox";
+console.log(upercaseOfFirstChar(newString));
